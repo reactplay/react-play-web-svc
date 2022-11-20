56dadcf7d48204f6dab60b689d1ae65ce1e61389
koustov <koustov@live.com>
2022-11-20 10:43:28 +0530

    write your own mechanism

--- services/template-provider/index.js
+++ services/template-provider/index.js
@@ -1,7 +1,32 @@
 const puppeteer = require("puppeteer");
+const PCR = require("puppeteer-chromium-resolver");
 
 const TakeScreenShot = async () => {
-  const browser = await puppeteer.launch();
+  const option = {
+    revision: "",
+    detectionPath: "",
+    folderName: ".chromium-browser-snapshots",
+    defaultHosts: [
+      "https://storage.googleapis.com",
+      "https://npm.taobao.org/mirrors",
+    ],
+    hosts: [],
+    cacheRevisions: 2,
+    retry: 3,
+    silent: false,
+  };
+  const stats = await PCR(option);
+  const browser = await stats.puppeteer
+    .launch({
+      headless: false,
+      args: ["--no-sandbox"],
+      executablePath: stats.executablePath,
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  // const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://www.freecodecamp.org/");
   const base64 = await page.screenshot({ encoding: "base64" });
