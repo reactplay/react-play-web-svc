921ea8f3f4a03a43bd298e721fefbad3b35aecc3
koustov <koustov@live.com>
2022-11-27 01:16:03 +0530

    sample image

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,4 +1,10 @@
 const { join } = require("path");
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
 
 /**
  * @type {import("puppeteer").Configuration}
