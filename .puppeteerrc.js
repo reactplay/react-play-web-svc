165f37a1da51124bb7a948206fb2f9fecca8a564
koustov <koustov@live.com>
2022-11-27 01:26:09 +0530

    sample image

--- .puppeteerrc.js
+++ .puppeteerrc.js
@@ -6,7 +6,7 @@ console.log("The dir path is : ", __dirname);
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: "/home/sbx_user1051/.cache/puppeteer", //join(__dirname, ".cache", "puppeteer"),
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
   experiments: {
     macArmChromiumEnabled: true,
   },
