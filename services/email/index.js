8eb8ac4f92a2ada5d39cfa98722d66168be1a33b
koustov <koustov@live.com>
2022-12-21 01:04:00 +0530

    trying redirect

--- services/email/index.js
+++ services/email/index.js
@@ -6,7 +6,6 @@ const EMAIL_TEMPLATE_ID = process.env.SENDGRID_EMAIL_TEMPLATE_ID;
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(SENDGRID_API_KEY);
 
-console.log(`API KEY: ${SENDGRID_API_KEY}`);
 const sendMail = (email, badge) => {
   if (email === "smahanta118@gmail.com" || email === "sansup49@gmail.com") {
     console.log(`Sending mail to : ${email} for ${badge}`);
